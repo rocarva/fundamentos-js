77543a2c75e2b7d9098b28151230613bdf3c0fa8
Rodrigo Carvalho <ro051186@hotmail.com>
2022-10-18 15:54:26 -0300

    bool

---
+++ type-boolean.js
@@ -0,0 +1,11 @@
+//boolean
+
+const primeroNumero = 5;
+const segundoNumero = 10;
+
+// console.log(primeroNumero === segundoNumero)
+
+const texto1 = "Alura"
+const texto2 = "a"
+
+console.log(texto1 === texto2) 
