9f1ece1c98a2d8dba6c48446fef99399d7d88a1b
Rodrigo Carvalho <ro051186@hotmail.com>
2022-10-18 15:43:09 -0300

    primeiros_codigos_js

---
+++ type-number.js
@@ -0,0 +1,28 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 3 ;
+
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante * pontoFlutuanteSemZero
+
+console.log(novaOperacao)
+
+const idade = 28
+const pi = 3.14
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
